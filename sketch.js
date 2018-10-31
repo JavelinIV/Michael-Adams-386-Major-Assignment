851d7acca9db047274bea41bc0b61b6cfacb24e0
Michael Adams <43168991+JavelinIV@users.noreply.github.com>
2018-10-31 23:40:37 +1300

    Add files via upload
    Final Version

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+//Michael Adams CMPO 385 Major Assignment
+// THE KEYBOARD THEREMIN
+
+//Variable Instantiation
 var loading;
 var button;
 var osc, env, effect;
@@ -15,6 +19,7 @@ var distortion
 var delay
 var song
 
+//Function which waits untill the sound file is loaded before drawing the start button
 function soundLoaded(song){
   button = createButton('Start!');
   button.position(width*0.45, height*0.75);
@@ -25,6 +30,7 @@ function soundLoaded(song){
   backingTrackFile.loop()
 }
 
+//Setup function which instatiates the background, colours, sounds, effects, and loads the mp3 file.
 function setup() {
   createCanvas(windowWidth,windowHeight);
   background(30, 68, 30)
@@ -79,17 +85,19 @@ function setup() {
   osc4.disconnect()
   osc4.connect(lowPassed)
   osc4.start();
-
   soundFormats('mp3','wav');
   backingTrackFile = loadSound('/assets/backingTrack.mp3', soundLoaded);
   backingTrackFile.setVolume(0.0);
 }
 
+//Draw function where the good stuff happens
 function draw() {
+  //if loading is true, load the instructions to the screen
   if(loading === true){
     background(25);
     strokeWeight(4);
     stroke(255);
+    //create 4 lines 10 squares with slight hue change incolour to represent the keyboard
     for(i = 0; i < 10; i++){
       fill(15, 119, 7)
       rect((i*(windowWidth*0.09)+(windowWidth*0.1)),(windowHeight*0.05), (windowWidth*0.06), (windowHeight*0.106))
@@ -100,6 +108,7 @@ function draw() {
       fill(117, 255, 107)
       rect((i*(windowWidth*0.09)+(windowWidth*0.11)),(windowHeight*0.50), (windowWidth*0.06), (windowHeight*0.106))
     }
+    //create more squares to represent the spacebar and arrow keys
     fill(155, 255, 147)
     rect((windowWidth*0.09),windowHeight*0.80, (windowWidth*0.33), (windowHeight*0.106))
     rect((windowWidth*0.7),windowHeight*0.68, (windowWidth*0.06), (windowHeight*0.106))
@@ -108,6 +117,7 @@ function draw() {
     rect((windowWidth*0.767),windowHeight*0.80, (windowWidth*0.06), (windowHeight*0.106))
     textSize(70)
     fill(54, 211, 42)
+    //Text which tells the user what is what
     text('PITCH', windowWidth*0.45, windowHeight*0.69);
     text('SUSTAIN', windowWidth*0.18, windowHeight*0.98);
     text('SOUND', windowWidth*0.67, windowHeight*0.98);
@@ -131,74 +141,79 @@ function draw() {
     text('🡣', windowWidth*0.712, windowHeight*0.88)
     text('🡠', windowWidth*0.645, windowHeight*0.88)
     text('🡢', windowWidth*0.779, windowHeight*0.88)
-
   }
   else{
+    //remove the button after it is pressed
     removeElements();
+    //draw the background every frame
     background(30, 68, 30);
+    //mute/unmute backing track
     if(backingTrack === true){
       backingTrackFile.setVolume(0.5);
     }
     else{
       backingTrackFile.setVolume(0.0);
     }
-    if( key === 'z'){      currentPositionX = 1;    currentPositionY = 1;    console.log(currentPositionX);  }
-    if( key === 'x'){      currentPositionX = 2;    currentPositionY = 1;    console.log(currentPositionX);  }
-    if( key === 'c'){      currentPositionX = 3;    currentPositionY = 1;    console.log(currentPositionX);  }
-    if( key === 'v'){      currentPositionX = 4;    currentPositionY = 1;    console.log(currentPositionX);  }
-    if( key === 'b'){      currentPositionX = 5;    currentPositionY = 1;    console.log(currentPositionX);  }
-    if( key === 'n'){      currentPositionX = 6;    currentPositionY = 1;    console.log(currentPositionX);  }
-    if( key === 'm'){      currentPositionX = 7;    currentPositionY = 1;    console.log(currentPositionX);  }
-    if( key === ','){      currentPositionX = 8;    currentPositionY = 1;    console.log(currentPositionX);  }
-    if( key === '.'){      currentPositionX = 9;    currentPositionY = 1;    console.log(currentPositionX);  }
-    if( key === '/'){      currentPositionX = 10;   currentPositionY = 1;    console.log(currentPositionX);  }
-    if( key === 'a'){      currentPositionX = 1;    currentPositionY = 2;    console.log(currentPositionX);  }
-    if( key === 's'){      currentPositionX = 2;    currentPositionY = 2;    console.log(currentPositionX);  }
-    if( key === 'd'){      currentPositionX = 3;    currentPositionY = 2;    console.log(currentPositionX);  }
-    if( key === 'f'){      currentPositionX = 4;    currentPositionY = 2;    console.log(currentPositionX);  }
-    if( key === 'g'){      currentPositionX = 5;    currentPositionY = 2;    console.log(currentPositionX);  }
-    if( key === 'h'){      currentPositionX = 6;    currentPositionY = 2;    console.log(currentPositionX);  }
-    if( key === 'j'){      currentPositionX = 7;    currentPositionY = 2;    console.log(currentPositionX);  }
-    if( key === 'k'){      currentPositionX = 8;    currentPositionY = 2;    console.log(currentPositionX);  }
-    if( key === 'l'){      currentPositionX = 9;    currentPositionY = 2;    console.log(currentPositionX);  }
-    if( key === ';'){      currentPositionX = 10;   currentPositionY = 2;    console.log(currentPositionX);  }
-    if( key === 'q'){      currentPositionX = 1;   currentPositionY = 3;    console.log(currentPositionX);  }
-    if( key === 'w'){      currentPositionX = 2;    currentPositionY = 3;    console.log(currentPositionX);  }
-    if( key === 'e'){      currentPositionX = 3;    currentPositionY = 3;    console.log(currentPositionX);  }
-    if( key === 'r'){      currentPositionX = 4;    currentPositionY = 3;    console.log(currentPositionX);  }
-    if( key === 't'){      currentPositionX = 5;    currentPositionY = 3;    console.log(currentPositionX);  }
-    if( key === 'y'){      currentPositionX = 6;    currentPositionY = 3;    console.log(currentPositionX);  }
-    if( key === 'u'){      currentPositionX = 7;    currentPositionY = 3;    console.log(currentPositionX);  }
-    if( key === 'i'){      currentPositionX = 8;    currentPositionY = 3;    console.log(currentPositionX);  }
-    if( key === 'o'){      currentPositionX = 9;    currentPositionY = 3;    console.log(currentPositionX);  }
-    if( key === 'p'){      currentPositionX = 10;   currentPositionY = 3;    console.log(currentPositionX);  }
-    if( key === '2'){      currentPositionX = 1;    currentPositionY = 4;    console.log(currentPositionX);  }
-    if( key === '3'){      currentPositionX = 2;    currentPositionY = 4;    console.log(currentPositionX);  }
-    if( key === '4'){      currentPositionX = 3;    currentPositionY = 4;    console.log(currentPositionX);  }
-    if( key === '5'){      currentPositionX = 4;    currentPositionY = 4;    console.log(currentPositionX);  }
-    if( key === '6'){      currentPositionX = 5;    currentPositionY = 4;    console.log(currentPositionX);  }
-    if( key === '7'){      currentPositionX = 6;    currentPositionY = 4;    console.log(currentPositionX);  }
-    if( key === '8'){      currentPositionX = 7;    currentPositionY = 4;    console.log(currentPositionX);  }
-    if( key === '9'){      currentPositionX = 8;    currentPositionY = 4;    console.log(currentPositionX);  }
-    if( key === '0'){      currentPositionX = 9;    currentPositionY = 4;    console.log(currentPositionX);  }
-    if( key === '-'){      currentPositionX = 10;   currentPositionY = 4;    console.log(currentPositionX);  }
-
+    //When *x* key is pressed change X and Y values to *n*
+    if( key === 'z'){      currentPositionX = 1;    currentPositionY = 1;}
+    if( key === 'x'){      currentPositionX = 2;    currentPositionY = 1;}
+    if( key === 'c'){      currentPositionX = 3;    currentPositionY = 1;}
+    if( key === 'v'){      currentPositionX = 4;    currentPositionY = 1;}
+    if( key === 'b'){      currentPositionX = 5;    currentPositionY = 1;}
+    if( key === 'n'){      currentPositionX = 6;    currentPositionY = 1;}
+    if( key === 'm'){      currentPositionX = 7;    currentPositionY = 1;}
+    if( key === ','){      currentPositionX = 8;    currentPositionY = 1;}
+    if( key === '.'){      currentPositionX = 9;    currentPositionY = 1;}
+    if( key === '/'){      currentPositionX = 10;   currentPositionY = 1;}
+    if( key === 'a'){      currentPositionX = 1;    currentPositionY = 2;}
+    if( key === 's'){      currentPositionX = 2;    currentPositionY = 2;}
+    if( key === 'd'){      currentPositionX = 3;    currentPositionY = 2;}
+    if( key === 'f'){      currentPositionX = 4;    currentPositionY = 2;}
+    if( key === 'g'){      currentPositionX = 5;    currentPositionY = 2;}
+    if( key === 'h'){      currentPositionX = 6;    currentPositionY = 2;}
+    if( key === 'j'){      currentPositionX = 7;    currentPositionY = 2;}
+    if( key === 'k'){      currentPositionX = 8;    currentPositionY = 2;}
+    if( key === 'l'){      currentPositionX = 9;    currentPositionY = 2;}
+    if( key === ';'){      currentPositionX = 10;   currentPositionY = 2;}
+    if( key === 'q'){      currentPositionX = 1;    currentPositionY = 3;}
+    if( key === 'w'){      currentPositionX = 2;    currentPositionY = 3;}
+    if( key === 'e'){      currentPositionX = 3;    currentPositionY = 3;}
+    if( key === 'r'){      currentPositionX = 4;    currentPositionY = 3;}
+    if( key === 't'){      currentPositionX = 5;    currentPositionY = 3;}
+    if( key === 'y'){      currentPositionX = 6;    currentPositionY = 3;}
+    if( key === 'u'){      currentPositionX = 7;    currentPositionY = 3;}
+    if( key === 'i'){      currentPositionX = 8;    currentPositionY = 3;}
+    if( key === 'o'){      currentPositionX = 9;    currentPositionY = 3;}
+    if( key === 'p'){      currentPositionX = 10;   currentPositionY = 3;}
+    if( key === '2'){      currentPositionX = 1;    currentPositionY = 4;}
+    if( key === '3'){      currentPositionX = 2;    currentPositionY = 4;}
+    if( key === '4'){      currentPositionX = 3;    currentPositionY = 4;}
+    if( key === '5'){      currentPositionX = 4;    currentPositionY = 4;}
+    if( key === '6'){      currentPositionX = 5;    currentPositionY = 4;}
+    if( key === '7'){      currentPositionX = 6;    currentPositionY = 4;}
+    if( key === '8'){      currentPositionX = 7;    currentPositionY = 4;}
+    if( key === '9'){      currentPositionX = 8;    currentPositionY = 4;}
+    if( key === '0'){      currentPositionX = 9;    currentPositionY = 4;}
+    if( key === '-'){      currentPositionX = 10;   currentPositionY = 4;}
+    //when arrow keys are pressed, assign a new string to the variable currentKey
     if( keyCode === UP_ARROW){   currentKey = 'UP'     }
     if( keyCode === DOWN_ARROW){  currentKey = 'DOWN'   }
     if( keyCode === LEFT_ARROW){   currentKey = 'LEFT'    }
     if( keyCode === RIGHT_ARROW){   currentKey = 'RIGHT'    }
-
+    //change the sustain value when true or false
     if(sustain === false){
       sustainValue = 0.5
     }
     if(sustain === true){
       sustainValue = 5
     }
+    //Change the freqencypitch dependent on the current pressed key
     osc1.freq(midiToFreq(scalePitch[currentPositionX]));
     osc2.freq(midiToFreq(scalePitch[currentPositionX]));
     osc3.freq(midiToFreq(scalePitch[currentPositionX]));
     osc4.freq(midiToFreq(scalePitch[currentPositionX]));
     osc4.freq(modulator1);
+    //set the volume and release dependent on the current pressed key and if the sustain has been toggled on or off
     env1.setADSR(0.01, 0.0, (dynamicLevel[currentPositionY])*0.01, sustainValue);
     env2.setADSR(0.01, 0.0, (dynamicLevel[currentPositionY]), sustainValue);
     env3.setADSR(0.01, 0.0, (dynamicLevel[currentPositionY]), sustainValue);
@@ -206,7 +221,8 @@ function draw() {
     ellipse(map(currentPositionX, 1, 10, 50, (windowWidth-50)), map(currentPositionY, 1,4, (windowHeight-50), 50), 95,95);
   }
 }
-
+// play through the envelope if a key is key is pressed
+//choose envelope dependent on the last pressed arrow key
 function keyPressed(){
   if( currentKey === 'UP'){
     env1.play();
@@ -224,15 +240,15 @@ function keyPressed(){
     sustain = !sustain
   }
 }
-
+//when start button is pressed change load state to false
 function changeLoadState() {
   loading = false;
 }
-
+//when double clicked mute or unmute the backing track
 function doubleClicked(){
   backingTrack = !backingTrack
 }
-
+//when window size changes, reesize the canvas to the new size
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
